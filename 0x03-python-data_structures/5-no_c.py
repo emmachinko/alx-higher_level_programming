26cff624bce331191f5bc4dbe88a41f01786f9ed
emmachinko <emmachris4603@gmail.com>
2023-12-03 00:55:49 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for char in listofchars:
         if char == 'c' or char == 'C':
             listofchars.remove(char)
-    return new_string
+    return("".join(listofchars))
