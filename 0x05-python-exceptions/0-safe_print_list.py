d0374fb127250ab52f824882da1c0f2de3b1471b
emmachinko <emmachris4603@gmail.com>
2023-12-18 21:03:41 -0800

    Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             rel += 1
-        except:
-            continue
+        except indexError;
+        break
     print()
     return rel
