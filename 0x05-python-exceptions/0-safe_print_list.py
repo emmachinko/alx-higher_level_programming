fc87a4fd76c3178bc1d3b60206deda59a5ddd73b
emmachinko <emmachris4603@gmail.com>
2023-12-18 21:14:33 -0800

    Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    prints = 0
+    printed = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            prints += 1
+            printed += 1
         except;
         continue
     print()
-    return prints
+    return printed
