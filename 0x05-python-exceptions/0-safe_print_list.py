40bfe0d09bd413caa35e8ad2203b19b1358df4a5
emmachinko <emmachris4603@gmail.com>
2023-12-18 20:56:44 -0800

    Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    rel = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            rel += 1
+        except:
+            continue
+    print()
+    return rel
